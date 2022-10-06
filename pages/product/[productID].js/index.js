bb61db04095cbb370fbeee8e64c0c735e9eacbda
Gilton <26347427+giltwizy@users.noreply.github.com>
2022-10-06 17:55:46 +0300

    scenario 3

--- pages/product/[productID].js/index.js
+++ pages/product/[productID].js/index.js
@@ -2,8 +2,8 @@ import { useRouter } from 'next/router'
 
 function ProductDetails(){
     const _route = useRouter()
-    const productID =_route.query.productID
-    return <h2>Details about the product</h2>
+    const produc =_route.query.productIDY
+    return <h2>Details about the product {produc}</h2>
 }
 
 export default ProductDetails
